e262291e7951e312532b3255b072fbe8f9b30162
mrudulbhatt <mrudul1203bhatt@gmail.com>
2020-04-10 18:57:37 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const firebase = require("firebase");
+require("firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB66PM1uVkvtSG29biYja4ju8R2OGnRaLM",
+  authDomain: "evernote-clone-79c4a.firebaseapp.com",
+  databaseURL: "https://evernote-clone-79c4a.firebaseio.com",
+  projectId: "evernote-clone-79c4a",
+  storageBucket: "evernote-clone-79c4a.appspot.com",
+  messagingSenderId: "1002699384514",
+  appId: "1:1002699384514:web:9442135dbe3105b17cbd5b",
+  measurementId: "G-QF1HVQS1K3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
