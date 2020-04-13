7696c39eabdce2d7107110825c246203fa4d2c75
mrudulbhatt <mrudul1203bhatt@gmail.com>
2020-04-13 08:21:09 +0530

    added new note create

--- src/App.js
+++ src/App.js
@@ -36,11 +36,53 @@ class App extends React.Component {
         <SidebarComponent
           notes={this.state.notes}
           selectedNoteIndex={this.state.selectedNoteIndex}
+          deleteNote={this.deleteNote}
+          selectNote={this.selectNote}
+          newNote={this.newNote}
         ></SidebarComponent>
-        <EditorComponent></EditorComponent>
+        {this.state.selectedNote ? (
+          <EditorComponent
+            selectedNote={this.state.selectedNote}
+            selectedNoteIndex={this.state.selectedNoteIndex}
+            notes={this.state.notes}
+            noteUpdate={this.noteUpdate}
+          ></EditorComponent>
+        ) : null}
       </div>
     );
   }
+
+  selectNote = (note, index) =>
+    this.setState({ selectedNoteIndex: index, selectedNote: note });
+
+  noteUpdate = (id, noteObj) => {
+    firebase.firestore().collection("notes").doc(id).update({
+      title: noteObj.title,
+      body: noteObj.body,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+  };
+
+  newNote = async (title) => {
+    const note = {
+      title: title,
+      body: "",
+    };
+    const newFromDB = await firebase.firestore().collection("notes").add({
+      title: note.title,
+      body: note.body,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    const newID = newFromDB.id;
+    await this.setState({ notes: [...this.state.notes, note] });
+    const newNoteIndex = this.state.notes.indexOf(
+      this.state.notes.filter((_note) => _note.id === newID)[0]
+    );
+    this.setState({
+      selectedNote: this.state.notes[newNoteIndex],
+      selectedNoteIndex: newNoteIndex,
+    });
+  };
 }
 
 export default App;
--- src/editor/editor.js
+++ src/editor/editor.js
@@ -15,6 +15,24 @@ class EditorComponent extends React.Component {
     };
   }
 
+  componentDidMount = () => {
+    this.setState({
+      text: this.props.selectedNote.body,
+      title: this.props.selectedNote.title,
+      id: this.props.selectedNote.id,
+    });
+  };
+
+  componentDidUpdate = () => {
+    if (this.props.selectedNote.id !== this.state.id) {
+      this.setState({
+        text: this.props.selectedNote.body,
+        title: this.props.selectedNote.title,
+        id: this.props.selectedNote.id,
+      });
+    }
+  };
+
   render() {
     const { classes } = this.props;
 
@@ -34,7 +52,10 @@ class EditorComponent extends React.Component {
   };
 
   update = debounce(() => {
-    console.log("updating database");
+    this.props.noteUpdate(this.state.id, {
+      title: this.state.title,
+      body: this.state.text,
+    });
   }, 1500);
 }
 
